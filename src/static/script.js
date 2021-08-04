a20f833ac23bcb478f264ef2f71a58382bdca1d2
lopar6 <lopar6@gmail.com>
2021-08-03 18:56:02 -0600

    fixed mountain-go-brrrr bug

--- src/static/script.js
+++ src/static/script.js
@@ -15,7 +15,7 @@ let offset5 = new Offset();
 
 let element = null;
 
-window.onload = function() {
+window.onload = () => {
     element = document.querySelector("#parallax");
     document.addEventListener("mousemove", horizontalParallax);
     setTimeout(() => {document.body.className += " loaded";}, 400);
@@ -23,17 +23,22 @@ window.onload = function() {
     setTimeout(() => {document.body.className += " timeMark2";}, 2200);
 }
 
-function horizontalParallax(event){
+horizontalParallax = (event) => {
     // len from side to mid point on screen
-    let x = window.innerWidth/2;
+    let x = window.outerWidth/2;
     // do not parallax on mobile
     if(x > 500){
         let mouseX = event.clientX;
+        // makes movement dynamically scale to zoom amount
+        let pixelRatio = window.devicePixelRatio;
         let percentOffset = 1;
-        // makes movement somewhat dynamically scale to screen size
-        if(x < 900){
-            percentOffset = .1
+        if (pixelRatio < 1) {
+            percentOffset =  (window.devicePixelRatio * .08);
         }
+        else if (pixelRatio > 1) {
+            percentOffset = .1;
+        }
+
         offset1.x = - (mouseX - x) * .25 * percentOffset;
         offset2.x = - (mouseX - x) * .20 * percentOffset;
         offset3.x = - (mouseX - x) * .15 * percentOffset;
@@ -43,6 +48,6 @@ function horizontalParallax(event){
     }
 }
 
-function applyParallax(){
+applyParallax = () => {
     element.style.backgroundPosition = `${offset1.x}% ${offset1.y}%, ${offset2.x}% ${offset2.y}%, ${offset3.x}% ${offset3.y}%, ${offset4.x}% ${offset4.y}%, ${offset5.x}% ${offset5.y}%`;
 }
